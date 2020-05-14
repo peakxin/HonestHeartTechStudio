887554f0eed02ba50092a45f26b82fbecf0877c2
Peak Xin <xinyflove@sina.com>
2020-05-14 16:18:57 +0800

    优化

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: Peak Xin 
+ * @Date: 2020-05-14 16:10:19 
+ * @Last Modified by: Peak Xin
+ * @Last Modified time: 2020-05-14 16:16:19
+ */
+$('body').scrollspy({ target: '.navbar-menu' })
