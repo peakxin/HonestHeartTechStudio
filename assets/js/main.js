138096ac9dc388b0ef7d55fc37f13e979937ea4e
Peak Xin <xinyflove@sina.com>
2020-05-15 00:26:00 +0800

    m

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,25 @@
  * @Author: Peak Xin 
  * @Date: 2020-05-14 16:10:19 
  * @Last Modified by: Peak Xin
- * @Last Modified time: 2020-05-14 16:16:19
+ * @Last Modified time: 2020-05-14 22:51:31
  */
 $('body').scrollspy({ target: '.navbar-menu' });
+
+var navBar = document.createElement('span');
+navBar.setAttribute('class', 'nav-bar');
+navBar.style.cssText = 'left: 0; top: 46px; width: 0px; opacity: 0;';
+$('.navbar-menu ul').append(navBar)
+    .on('mouseover', 'li', function(e) {
+        var obj = $(e.target);
+        var navBarUlLeft = $('.navbar-menu ul').offset().left;
+        var width = obj.outerWidth();
+        var left = obj.offset().left - navBarUlLeft;
+        navBar.style.cssText = 'left: '+left+'px; top: 46px; width: '+width+'px; opacity: 1;';
+    })
+    .on('mouseout', 'li', function(e) {
+        var obj = $(e.target);
+        var navBarUlLeft = $('.navbar-menu ul').offset().left;
+        var width = obj.outerWidth();
+        var left = obj.offset().left - navBarUlLeft + width/2;
+        navBar.style.cssText = 'left: '+left+'px; top: 46px; width: 0; opacity: 0;';
+    });
